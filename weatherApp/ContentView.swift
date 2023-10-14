d31e0e63196b0fa825e600e1b259e0222027ace4
S-Guettner <sven_guettner@protonmail.com>
2023-10-15 01:57:48 +0200

    getting data

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var weatherManager = WeatherNetworkManager.shared
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("git test")
+            if let weatherData = weatherManager.weatherData {
+                Text("City: \(weatherData.location.name)")
+                Text("Region: \(weatherData.location.region)")
+                Text("Country: \(weatherData.location.country)")
+                Text("Temperature: \(weatherData.current.temp_c)°C")
+                Text("Condition: \(weatherData.current.condition.text)")
+            } else {
+                Text("Loading...")
+            }
+        }
+        .onAppear {
+            // Immediately fetch data when the view appears
+            self.weatherManager.fetchData(for: "Berlin") { data in
+                DispatchQueue.main.async {
+                    self.weatherManager.weatherData = data
+                }
+            }
         }
         .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
---
+++ weatherApp/Managers/weatherData.swift
@@ -0,0 +1,50 @@
+import Foundation
+import Combine
+
+class WeatherNetworkManager: ObservableObject { // Conform to ObservableObject
+    
+    static let shared = WeatherNetworkManager()
+    
+    @Published var weatherData: WeatherData? // Add @Published to properties that should trigger updates
+    
+    struct WeatherData: Codable {
+        var location: Location
+        var current: CurrentWeather
+    }
+
+    struct Location: Codable {
+        var name: String
+        var region: String
+        var country: String
+    }
+
+    struct CurrentWeather: Codable {
+        var temp_c: Double
+        var condition: Condition
+    }
+
+    struct Condition: Codable {
+        var text: String
+        var icon: String
+    }
+    
+    func fetchData(for city: String, completion: @escaping (WeatherData?) -> Void) {
+        if let url = URL(string: "https://api.weatherapi.com/v1/current.json?key=7a423cfefcf34dff85e222625231410&q=/\(city)&aqi=no") {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let decodedData = try decoder.decode(WeatherData.self, from: data)
+                        
+                        // Update the @Published property to trigger view updates
+                        DispatchQueue.main.async {
+                            self.weatherData = decodedData
+                        }
+                    } catch {
+                        print("Error decoding JSON: \(error)")
+                    }
+                }
+            }.resume()
+        }
+    }
+}
