021d8d7c5a6c606ebba8e3a1d15d868826a31b23
S-Guettner <sven_guettner@protonmail.com>
2023-10-15 00:10:31 +0200

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Sven Güttner on 15.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
