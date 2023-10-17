d84ff7e983ec3c5ae6d3127dbcf71b7168d0bccb
S-Guettner <sven_guettner@protonmail.com>
2023-10-17 14:58:48 +0200

    styling

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -23,36 +23,65 @@ struct ContentView: View {
     
     //Body
     var body: some View {
-        VStack {
-            
-            HStack {
-                TextField("City", text: $city)
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1))
-                    .cornerRadius(10)
-                    .onChange(of: city, initial: true) {
-                        self.weatherManager.fetchData(for: city) { data in
-                            DispatchQueue.main.async {
-                                self.weatherManager.weatherData = data
+        HStack{
+            VStack {
+                AsyncImage(url: URL(string: "https://source.unsplash.com/random/250x250/?\(city)"), scale: 1) { phase in
+                    switch phase {
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .frame(width: 370, height: 250)
+                                .clipShape(RoundedRectangle(cornerRadius: 25))
+                                
+                                
+                        case .failure(_):
+                            ProgressView()
+                            .frame(width: 250, height: 250)
+                        case .empty:
+                            ProgressView() // Loading indicator
+                        @unknown default:
+                            ProgressView() // A generic loading indicator for unknown cases
+                    }
+                }
+                Spacer()
+
+
+
+
+                    
+                HStack {
+                    
+                    TextField("City", text: $city)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1))
+                        .cornerRadius(10)
+                        .onChange(of: city, initial: true) {
+                            self.weatherManager.fetchData(for: city) { data in
+                                DispatchQueue.main.async {
+                                    self.weatherManager.weatherData = data
+                                }
                             }
                         }
-                    }
 
+                }
+                .foregroundColor(.black)
+                
+                if let weatherData = weatherManager.weatherData {
+                    Text("\(weatherData.location.name)")
+                    Text("\(weatherData.location.country)")
+                    Text("\(temperature)°C")
+                    Text("Condition: \(weatherData.current.condition.text)")
+                } else {
+                    Text("Loading...")
+                }
             }
-            .foregroundColor(.black)
-            
-            if let weatherData = weatherManager.weatherData {
-                Text("\(weatherData.location.name)")
-                Text("\(weatherData.location.country)")
-                Text("\(temperature)°C")
-                Text("Condition: \(weatherData.current.condition.text)")
-            } else {
-                Text("Loading...")
-            }
+
+            .padding()
         }
+        .frame(height: 500)
+        }
+        
 
-        .padding()
-    }
 }
 #Preview {
     ContentView()
