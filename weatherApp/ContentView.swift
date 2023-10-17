7fd447c0e10247dfcc20812785bac917322580bb
S-Guettner <sven_guettner@protonmail.com>
2023-10-17 13:14:41 +0200

    Changing City Name now triggers new fetch

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Variables
     @ObservedObject var weatherManager = WeatherNetworkManager.shared
-
+    @State private var city: String = "Berlin"
+    
+    //Body
     var body: some View {
         VStack {
+            
+            HStack {
+                TextField("City", text: $city)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1))
+                    .cornerRadius(10)
+                    .onChange(of: city, initial: true) {
+                        self.weatherManager.fetchData(for: city) { data in
+                            DispatchQueue.main.async {
+                                self.weatherManager.weatherData = data
+                            }
+                        }
+                    }
+
+            }
+            .foregroundColor(.black)
+            
             if let weatherData = weatherManager.weatherData {
                 Text("City: \(weatherData.location.name)")
                 Text("Region: \(weatherData.location.region)")
@@ -22,14 +42,7 @@ struct ContentView: View {
                 Text("Loading...")
             }
         }
-        .onAppear {
-            // Immediately fetch data when the view appears
-            self.weatherManager.fetchData(for: "Berlin") { data in
-                DispatchQueue.main.async {
-                    self.weatherManager.weatherData = data
-                }
-            }
-        }
+
         .padding()
     }
 }
